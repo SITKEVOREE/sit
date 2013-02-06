0353f6beea3e343bec36c5d0cf047d307c7ccdc5
JeD <jedartois@gmail.com>
2013-02-06 13:52:48 +0100

    add example

---
+++ android/org.kevoreetp.android.demo/src/main/java/org/kevoreetp/android/demo/FakeComponent.java
@@ -0,0 +1,37 @@
+package org.kevoreetp.android.demo;
+
+import org.kevoree.annotation.*;
+import org.kevoree.framework.AbstractComponentType;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jed
+ * Date: 29/01/13
+ * Time: 16:47
+ * To change this template use File | Settings | File Templates.
+ */
+@Library(name = "Android")
+@ComponentType
+public class FakeComponent  extends AbstractComponentType {
+
+
+    @Start
+    public void start() {
+
+
+    }
+
+
+    @Stop
+    public void stop() {
+
+
+    }
+
+    @Update
+    public void update() {
+
+
+    }
+
+}
